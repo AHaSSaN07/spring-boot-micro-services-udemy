c31d087200c304181bb5573079c301309915f255
AHaSSaN07 <enga.hassan60@gmail.com>
2021-06-12 22:24:07 +0200

    added profiles

--- UsersMicroService/src/main/java/com/hassan/PhotoApp/UsersMicroService/UsersMicroServiceApplication.java
+++ UsersMicroService/src/main/java/com/hassan/PhotoApp/UsersMicroService/UsersMicroServiceApplication.java
@@ -1,37 +1,60 @@
 package com.hassan.PhotoApp.UsersMicroService;
 
 import feign.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.env.Environment;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.client.RestTemplate;
+
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
 public class UsersMicroServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(UsersMicroServiceApplication.class, args);
-	}
-	@Bean
-	public BCryptPasswordEncoder BCryptPasswordEncoder(){
-		return new BCryptPasswordEncoder();
-	}
 
-	@Bean
-	@LoadBalanced
-	public RestTemplate getRestTemplate(){
-		return new RestTemplate();
-	}
+    @Autowired
+	Environment environment;
 
-	@Bean
-	Logger.Level feignLogger(){
-		return Logger.Level.FULL;
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(UsersMicroServiceApplication.class, args);
+    }
+
+    @Bean
+    public BCryptPasswordEncoder BCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate getRestTemplate() {
+        return new RestTemplate();
+    }
+
+    @Bean
+    Logger.Level feignLogger() {
+        return Logger.Level.FULL;
+    }
+
+    @Bean
+    @Profile("production")
+    public String createProdBean() {
+		System.out.println("crated production bean wth env : " + environment.getProperty("myapp.env"));
+        return "production bean";
+    }
+
+    @Bean
+    @Profile("default")
+    public String createDevBean() {
+		System.out.println("crated dev bean wth env : " + environment.getProperty("myapp.env"));
+
+		return "production bean";
+    }
 }

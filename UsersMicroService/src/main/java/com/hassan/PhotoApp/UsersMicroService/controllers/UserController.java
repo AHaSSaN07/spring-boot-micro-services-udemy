c946b24e8fa061aacadf11e360c030eaf34c8160
AHaSSaN07 <enga.hassan60@gmail.com>
2021-06-24 16:06:13 +0200

    changed dir name

--- UsersMicroService/src/main/java/com/hassan/PhotoApp/UsersMicroService/controllers/UserController.java
+++ UsersMicroService/src/main/java/com/hassan/PhotoApp/UsersMicroService/controllers/UserController.java
@@ -14,6 +14,7 @@ import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -44,6 +45,8 @@ public class UserController {
         return new ResponseEntity(modelMapper.map(addedUser, UserResponseModel.class), HttpStatus.CREATED);
     }
 
+
+    //@PreAuthorize("principal = #userId")
     @GetMapping(value = "/{userId}", consumes = {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE},
             produces = {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE})
     public ResponseEntity getUsers(@PathVariable("userId") String userId){
